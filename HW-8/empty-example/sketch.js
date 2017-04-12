23d1e1dc40874d9fc782d798b6bd9d58bc8931cd
sweber20 <sweber1080@gmail.com>
2017-04-12 16:20:58 -0600

    8
    finished 8

---
+++ HW-8/empty-example/sketch.js
@@ -0,0 +1,55 @@
+var stars = []
+var xR = 350
+var xT2 = 375
+var xTT1 = 400
+var xTT2 = 360
+var xTT3 = 390
+
+function setup() {
+createCanvas(windowWidth,windowHeight);
+  for (var i = 0; i < 50; i++) {
+    var size = random(5, 10);
+    var acc = map(size, 5, 10, 0.1, 0.05);
+    stars.push( new Star(random(width), random(height),  size, acc   ))
+}
+}
+
+
+function draw() {
+
+  push();
+  background(0);
+  fill(32, 55, 108);
+  rect(xR,200, 50, 200);
+  xR += random(-1, 1);
+
+  pop();
+
+  push();
+  fill(255);
+  triangle(xR,200,xTT1,200,xT2,150);
+    // xR += random(-1, 1);
+    // xTT1 += random(-1, 1);
+    // xT2 += random(-1, 1);
+  pop();
+
+  push();
+  fill(200,20,20);
+  triangle(xT2, 400,xTT2, 450,xR,400);
+  triangle(xT2, 400,xTT3, 450,xTT1,400);
+  // xR += random(-1, 1);
+  // xTT1 += random(-1, 1);
+  // xT2 += random(-1, 1);
+  // xTT2 += random (-1, 1);
+
+  pop();
+
+
+    for (var i = 0; i < stars.length; i++) {
+     stars[i].display()
+
+}
+
+
+
+}
