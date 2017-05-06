0ade1c34f6673e66dc330eb314f876a14031c7b6
sweber20 <sweber1080@gmail.com>
2017-05-05 21:24:18 -0600

    almost finished

--- Final/empty-example/myClass.js
+++ Final/empty-example/myClass.js
@@ -1,10 +1,55 @@
 
 
 
-function Gsq(init_X, init_Y, size) {
+function Gsq(init_X, init_Y, size, color) {
   this.x = init_X;
   this.y = init_Y;
   this.size = size;
+  this.color = color;
+
+  // this.alpha =
+  // this.buzzer = ellipse(this.x, this.y, 75, 75)
+  this.imPressed = false
+  this.simonpressed = false
+
+
+
+}
+
+Gsq.prototype.frame = function () {
+  this.display();
+  this.buttonPressed();
+  // this.btWhenPressed();
+}
+
+
+Gsq.prototype.display = function () {
+
+
+push();
+
+
+if ( this.imPressed || this.simonpressed ){
+  fill(255);
+
+} else {
+fill(this.color);
+}
+translate(this.x, this.y);
+rect(0, 0, 100,100);
+
+// rect(width / 2 + 400 ,height / 2 - 150 ,100,300);
+
+pop();
+
+
+
+
+
+};
+
+Gsq.prototype.btWhenPressed = function (){
+fill(255)
 
 
 
@@ -12,16 +57,27 @@ function Gsq(init_X, init_Y, size) {
 
 }
 
-Gsq.prototype.display = function () {
+Gsq.prototype.buttonPressed = function (){
+  if (
+    mouseIsPressed &&
+    mouseX > this.x &&
+    mouseX < this.x + 100 &&
+    mouseY > this.y &&
+    mouseY < this.y + 100
+  ) {
+    // true
+    // console.log("button pressed");
+    this.imPressed = true
+    return true;
+
+  }
+  else{
+    this.imPressed = false
+    return false;
+}
 
 
-push()
-translate(this.x,this.y);
-rect(700,700,100,100);
-fill(200, 0, 0);
-
-pop()
 
 
 
-};
+}
--- Final/empty-example/sketch.js
+++ Final/empty-example/sketch.js
@@ -1,13 +1,32 @@
+//things i still need:
+//resize to fit window
+//sounds?
+//title
+
+
+var bt1 = []
+var buttColors = ['rgba(255, 227, 20, 200)', 'rgba(255, 62, 137, 200)', 'rgba(30, 215, 0, 200)', 'rgba(255, 63, 33, 200)'];
+var placeholder = [];
+var simonsTurn = true;
+var correctPress = false;
+var simonNum = -1;
+var gameOver = false
+var score = 0
 
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
-  for (var i = 0; i < 1; i++) {
-     var size = random(5, 10);
+  createCanvas(windowWidth,windowHeight);
+  for (var i = 0; i < 4; i++) {
+    var size = random(5, 10);
     // var acc = map(size, 5, 10, 0.1, 0.05);
-    Gsqs.push( new Gsqs)
-}
+    // bt1.push( new Gsq(i *200 + 50, 300, size, buttColors[i]))
+  }
 
+  bt1.push( new Gsq(200, 200, size, 'rgba(255, 227, 20, 200)'))
+  bt1.push( new Gsq(300, 200, size, 'rgba(255, 62, 137, 200)'))
+  bt1.push( new Gsq(200 , 300, size, 'rgba(30, 215, 0, 200)'))
+  bt1.push( new Gsq(300, 300, size, 'rgba(255, 63, 33, 200)'))
+  placeholder.push(floor(random(4)));
 }
 
 
@@ -15,16 +34,102 @@ function draw() {
 
   push();
   background(0);
-
+  // frameCount()
   pop();
 
 
+  // execute simons turn
+  if (simonsTurn) {
 
-    for (var i = 0; i < Gsqs.length; i++) {
-     Gsqs[i].display()
+    background(255)
+    for (var i = 0; i < bt1.length; i++) {
+      bt1[i].frame()
+    }
+
+    // execute players turn
+  } else {
+
+    for (var i = 0; i < bt1.length; i++) {
+      bt1[i].frame()
+
+
+      console.log("button: " + placeholder[simonNum]);
+      if (bt1[i].buttonPressed() && i == placeholder[simonNum]){
+        correctPress = true;
+        console.log(i + ' - correct press');
+      }
+
+
+    }
+
+    stroke(0,255,0);
+    text(placeholder.length, 50, 50);
+
+
+    //  if (gameOver)
+    // if (!correctPress)
+    // {
+    //   score = 0
+    //   placeholder.lenth = 0
+    // }
+  }
+
+  if (frameCount %  120 == 0){
+
+    background(255);
+
+    if (!simonsTurn) {
+
+      if ( correctPress) {
+        simonNum++
+        console.log(simonNum);
+        correctPress = false
+        console.log("Keep Going: " + simonNum);
+      } else {
+        gameOver = true;
+        console.log("Game Over");
+      }
+    }
+
+    console.log("why")
+    console.log(placeholder)
+    console.log(simonNum)
+
+    if (gameOver) {
+      placeholder = [];
+      simonsTurn = true;
+
+      gameOver = false;
+      // simonpressed = true
+      simonNum = -1
+      addsquare()
+    } else {
+      if (simonNum >= 0 && simonNum < placeholder.length && simonsTurn) {
+        bt1[placeholder[simonNum]].simonpressed = false;
+      }
+      simonNum = ( simonNum + 1 );
+      if (simonNum >= 0 && simonNum < placeholder.length && simonsTurn) {
+        bt1[placeholder[simonNum]].simonpressed = true;
+      }
+      console.log(simonNum)
+      if (simonNum >= placeholder.length && simonsTurn) {
+        simonsTurn = false;
+        simonNum = 0;
+      }
+
+      else if (simonNum>= placeholder.length && !simonsTurn && !gameOver) {
+        simonsTurn = true;
+        simonNum = -1;
+        addsquare()
+      }
+    }
+
+  }
+
 
 }
 
 
-
+function addsquare(){
+  placeholder.push(floor(random(4)))
 }
